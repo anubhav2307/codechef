777f74ab050d0136180505af2f3c203f3efb8384
anubhav2307 <anubhavsingh2307@gmail.com>
2021-06-19 00:55:29 +0530

    .

---
+++ codeshef.py
@@ -0,0 +1,26 @@
+
+
+
+def check(l):
+	for x in l:
+		if x[0]<1 or x[0]>10**18 or len(x[1])<1 or len(x[1])>10**5 or not x[1].islower():
+			return False
+	return True
+
+
+
+
+if __name__ == '__main__':
+	l = []
+	T = int(input('enter the vallue of T: '))
+	for x in range(0,T):
+		N,S = input('enter the vallue of N and S: ').split()
+		l.append([int(N),S])
+	print(l)
+
+	if (T>=1 and T<=50) and check(l) :
+		print('okay to go')
+	else:
+		print('not okay to go')
+
+
